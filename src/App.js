2c14f7d4110e714fcb5d69022eb4cd6afc77bc6b
YQS <41525397+TomStone76@users.noreply.github.com>
2019-01-06 17:34:38 -0500

    Commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,71 @@
 import React, { Component } from 'react';
 import ChatMessage from './Components/ChatMessage';
 import Signup from './Components/Signup';
+import ChatApp from './Components/ChatApp';
+import { default as Chatkit } from '@pusher/chatkit-server';
+
+const chatkit = new Chatkit ({
+  instanceLocator: "v1:us1:79a82f98-922b-4900-984a-e22b0619b527",
+  key: "ea990234-bee3-40a1-9ffc-46c028748125:ISiUrcveffdDM77zO3TYe37AAJzXws2psME+ShAZlS0="
+})
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentView: 'ChatMessage'
+      currentUsername: '',
+      currentId: '',
+      currentView: 'signup'
     }
     this.changeView = this.changeView.bind(this);
+    this.createUser = this.createUser.bind(this);
   }
 
-  changeView(view) {
-    this.setState({
-      currentView: view
+  createUser(username) {
+    chatkit.createUser({
+      id: username,
+      name: username,
     })
+    .then((currentUser) => {
+      this.setState({
+        currentUsername: username,
+        currentId: username,
+        currentView: 'chatApp'
+      })
+    }).catch((err) => {
+            if(err.status = 400) {
+          this.setState({
+              currentUsername: username,
+              currentId: username,
+              currentView: 'chatApp'
+          })
+        } else {
+            console.log(err.status);
+        }
+      });
+    }
+
+  changeView(view) {
+      this.setState({
+        currentView: view
+      })
   }
 
   render() {
-    let view = '';
-    if (this.state.currentView === "ChatMessage") {
-      view = <ChatMessage changeView={this.changeView}/>
-    }
-    return (
-      <div className="App">
-          {view}
-      </div>
-    );
+        let view = '';
+
+        if (this.state.currentView === "ChatMessage") {
+            view = <ChatMessage changView={this.changeView}/>
+        } else if (this.state.currentView === "signup") {
+            view = <Signup onSubmit={this.createUser}/>
+        } else if (this.state.currentView === 'chatApp') {
+            view = <ChatApp currentId={this.state.currentId}/>  
+        }
+        return (
+          <div className="App">
+              {view}
+          </div>
+        )
   }
 }
 
